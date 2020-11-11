f03267d0b709b3fce0f930a51ccceed1b08aec5e
Álvaro Zájara <alzca1@gmail.com>
2020-11-11 18:54:09 +0100

    added subject to auth.service.ts for signin messages

--- src/app/guards/auth-guard.service.ts
+++ src/app/guards/auth-guard.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class CustomAuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {}
 
-  canActivate(route, state: RouterStateSnapshot) {
-    const isLoggedIn = this.auth.isLoggedIn();
-    if (isLoggedIn) {
-      return true;
-    }
-    this.router.navigate(['user/login'], {
-      queryParams: { returnUrl: state.url },
-    });
-    return false;
+  canActivate(route:ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    console.log(route, state)
+    return this.auth.user$.pipe(
+      map((user) => {
+        if (user) return true;
+
+        this.router.navigate(['user/login'], {queryParams: { returnUrl: state.url}});
+        return false;
+      })
+    );
   }
 }
